a3e805d7c2f732fad4278da2b525255272558d3b
Dzaky Mohammad <63341994+Dzaakk@users.noreply.github.com>
2023-03-12 19:55:30 +0700

    Race Condition

--- 03_Go Goroutines/goroutines_test/channel_test.go
+++ 03_Go Goroutines/goroutines_test/channel_test.go
@@ -118,9 +118,25 @@ func TestSelectChannel(t *testing.T) {
 		case data := <-channel2:
 			fmt.Println("Data dari Channel 2", data)
 			counter++
+		default:
+			fmt.Println("Menunggu Data")
 		}
 		if counter == 2 {
 			break
 		}
 	}
+}
+
+// Race Condition
+func TestRaceCondition(t *testing.T) {
+	x :=0 
+	for i := 1 ; i <= 1000; i++ {
+		go func ()  {
+			for j := 1; j < 100; j++ {
+				x=x+1
+			}
+		}()
+	}
+	time.Sleep(5 * time.Second)
+	fmt.Println("Counter = ", x)
 }
