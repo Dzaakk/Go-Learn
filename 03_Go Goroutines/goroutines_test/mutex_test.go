b6b7dd295af67d11a09d92096cb3a8dbdf94b7cd
Dzaky Mohammad <63341994+Dzaakk@users.noreply.github.com>
2023-03-12 21:02:48 +0700

    Deadlock

--- 03_Go Goroutines/goroutines_test/mutex_test.go
+++ 03_Go Goroutines/goroutines_test/mutex_test.go
@@ -37,12 +37,14 @@ type BankAccount struct {
 	Balance int
 }
 
+//read
 func (account *BankAccount) AddBalance(amount int) {
 	account.RWMUTEX.Lock()
 	account.Balance = account.Balance + amount
 	account.RWMUTEX.Unlock()
 }
 
+//write
 func (account *BankAccount) GetBalance() int {
 	account.RWMUTEX.RLock()
 	balance := account.Balance
@@ -53,15 +55,67 @@ func (account *BankAccount) GetBalance() int {
 func TestRWMutex(t *testing.T) {
 	account := BankAccount{}
 
-	for i := 0; i <100; i++ {
-		go func ()  {
-			for j := 0; j <100; j++{
+	for i := 0; i < 100; i++ {
+		go func() {
+			for j := 0; j < 100; j++ {
 				account.AddBalance(1)
 				fmt.Println(account.GetBalance())
 			}
 		}()
 	}
 
-	time.Sleep(5 *time.Second)
+	time.Sleep(5 * time.Second)
 	fmt.Println("Total Balance", account.GetBalance())
+}
+
+//Deadlock
+type UserBalance struct {
+	sync.Mutex
+	Name    string
+	Balance int
+}
+
+func (user *UserBalance) Lock() {
+	user.Mutex.Lock()
+}
+
+func (user *UserBalance) Unlock() {
+	user.Mutex.Unlock()
+}
+
+func (user *UserBalance) Change(amount int) {
+	user.Balance = user.Balance + amount
+}
+
+func Transfer(user1 *UserBalance, user2 *UserBalance, amount int) {
+	user1.Lock()
+	fmt.Println("Lock User1", user1.Name)
+	user1.Change(-amount)
+
+	time.Sleep(1 * time.Second)
+
+	user2.Lock()
+	fmt.Println("Lock User2", user2.Name)
+	user2.Change(amount)
+
+	user1.Unlock()
+	user2.Unlock()
+}
+
+func TestDeadlock(t *testing.T) {
+	user1 := UserBalance{
+		Name:    "tono",
+		Balance: 50000,
+	}
+	user2 := UserBalance{
+		Name:    "hana",
+		Balance: 40000,
+	}
+	
+	go Transfer(&user1, &user2, 1000)
+	go Transfer(&user2, &user1, 2000) //Terjadi deadlock dan hanya akan lock user1
+	time.Sleep(3 * time.Second)
+
+	fmt.Println("User ", user1.Name, ", Balance ", user1.Balance)
+	fmt.Println("User ", user2.Name, ", Balance ", user2.Balance)
 }
