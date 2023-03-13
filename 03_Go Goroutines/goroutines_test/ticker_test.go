14dc5ffbb14f82a386355773a18e436fefedda2a
Dzaky Mohammad <63341994+Dzaakk@users.noreply.github.com>
2023-03-13 23:56:09 +0700

    Ticker

--- 03_Go Goroutines/goroutines_test/ticker_test.go
+++ 03_Go Goroutines/goroutines_test/ticker_test.go
@@ -2,9 +2,29 @@ package goroutines_test
 
 import (
 	"fmt"
-	"sync"
 	"testing"
 	"time"
 )
+//gunakan ticker untuk aksi yang berulang
+func TestTicker(t *testing.T) {
+	ticker := time.NewTicker(1 * time.Second)
 
-func 
+	go func() {
+		time.Sleep(5 * time.Second)
+		ticker.Stop()
+	}()
+	//ambil data dari channel
+	for time := range ticker.C {
+		fmt.Println(time)
+	} 
+}
+
+func TestTick(t *testing.T) {
+	channel := time.Tick(1 * time.Second)
+	
+	for time := range channel{
+		fmt.Println(time)
+	}
+}
+
+
