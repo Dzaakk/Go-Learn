a8c3332e38d23d5c8d7688cd7bdc678a48922481
Dzaky Mohammad <63341994+Dzaakk@users.noreply.github.com>
2023-03-15 00:26:57 +0700

    Context with Cancel

--- 04_Go Context/Context/context_test.go
+++ 04_Go Context/Context/context_test.go
@@ -3,7 +3,9 @@ package context
 import (
 	"context"
 	"fmt"
+	"runtime"
 	"testing"
+	"time"
 )
 
 func TestContext(t *testing.T) {
@@ -15,6 +17,7 @@ func TestContext(t *testing.T) {
 	fmt.Println(todo)
 }
 
+//context with value
 func TestContextWithValue(t *testing.T) {
 	contextA := context.Background()
 
@@ -25,7 +28,7 @@ func TestContextWithValue(t *testing.T) {
 	contextE := context.WithValue(contextB, "e", "E")
 
 	contextF := context.WithValue(contextC, "f", "F")
-	contextG:= context.WithValue(contextF, "g", "G")
+	contextG := context.WithValue(contextF, "g", "G")
 
 	fmt.Println(contextA)
 	fmt.Println(contextB)
@@ -39,4 +42,72 @@ func TestContextWithValue(t *testing.T) {
 	fmt.Println(contextF.Value("c"))
 	fmt.Println(contextF.Value("b"))
 	fmt.Println(contextA.Value("b"))
+}
+
+func CreateCounterLeak() chan int {
+	destination := make(chan int)
+
+	go func() {
+		defer close(destination)
+		counter := 1
+		for {
+			destination <- counter
+			counter++
+		}
+	}()
+	return destination
+}
+func TestContextWithCancelLeak(t *testing.T) {
+	fmt.Println("Total Goroutine Awal : ", runtime.NumGoroutine())
+
+	destination := CreateCounterLeak()
+	fmt.Println("Total Goroutine Ketika Counter Berjalan : ", runtime.NumGoroutine())
+	for n := range destination {
+		fmt.Println("counter", n)
+		if n == 10 {
+			break
+		}
+	}
+	fmt.Println("Total Goroutine Setelah Proses Selesai : ", runtime.NumGoroutine())
+}
+
+//Not leak
+func CreateCounterNotLeak(ctx context.Context) chan int {
+	destination := make(chan int)
+
+	go func() {
+		defer close(destination)
+		counter := 1
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			default:
+				destination <- counter
+				counter++
+			}
+		}
+	}()
+	return destination
+}
+
+func TestContextWithCancelNotLeak(t *testing.T) {
+	fmt.Println("Total Goroutine Awal : ", runtime.NumGoroutine())
+
+	parent := context.Background()
+	ctx, cancel := context.WithCancel(parent)
+
+	destination := CreateCounterNotLeak(ctx)
+	fmt.Println("Total Goroutine Ketika Counter Berjalan : ", runtime.NumGoroutine())
+	for n := range destination {
+		fmt.Println("counter", n)
+		if n == 10 {
+			break
+		}
+	}
+	cancel()
+	//mengirim sinyal cancel ke context
+
+	time.Sleep(1 * time.Second)
+	fmt.Println("Total Goroutine Setelah Proses Selesai : ", runtime.NumGoroutine())
 }
