1a26db45a9fc8ce32f7622411122cf7fd07883c3
Dzaky Mohammad <63341994+Dzaakk@users.noreply.github.com>
2023-03-15 23:00:44 +0700

    Query SQL

--- 05_Go Database/database/sql_test.go
+++ 05_Go Database/database/sql_test.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 	"testing"
 )
-
+//eksekusi bukan query
 func TestExecSql(t *testing.T) {
 	db := GetConnection()
 	defer db.Close()
@@ -18,4 +18,28 @@ func TestExecSql(t *testing.T) {
 		panic(err)
 	}
 	fmt.Println("Success Insert New Customer")
+}
+//query context untuk eksekusi
+func TestQuerySQL(t *testing.T) {
+	db := GetConnection()
+	defer db.Close()
+
+	ctx := context.Background()
+
+	script := "SELECT id, name FROM customer"
+	rows, err := db.QueryContext(ctx, script)
+	if err != nil {
+		panic(err)
+	}
+	defer rows.Close()
+
+	for rows.Next(){
+		var id, name string
+		err = rows.Scan(&id, &name)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println("Id:", id)
+		fmt.Println("Name:", name)
+	}
 }
