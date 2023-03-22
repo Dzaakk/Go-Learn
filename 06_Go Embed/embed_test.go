a8baa0e424659e73b04fd81ecba29b47e5919dbb
Dzaky Mohammad <63341994+Dzaakk@users.noreply.github.com>
2023-03-23 00:02:57 +0700

    Embed File to Byte

--- 06_Go Embed/embed_test.go
+++ 06_Go Embed/embed_test.go
@@ -3,6 +3,8 @@ package golang_embed
 import (
 	_ "embed"
 	"fmt"
+	"io/fs"
+	"io/ioutil"
 	"testing"
 )
 
@@ -11,4 +13,13 @@ var version string
 
 func TestString(t *testing.T) {
 	fmt.Println(version)
+}
+
+var logo []byte
+
+func TestByte(t *testing.T) {
+	err := ioutil.WriteFile("Logo_new..png", logo, fs.ModePerm)
+	if err != nil {
+		panic(err)
+	}
 }
