8bbef446db0df516fca76503d9325750e69bac9e
Dzaky Mohammad <63341994+Dzaakk@users.noreply.github.com>
2023-03-24 06:19:14 +0700

    Serve Mux

--- 07_Go Web/handler_test.go
+++ 07_Go Web/handler_test.go
@@ -11,10 +11,28 @@ func TestHandler(t *testing.T) {
 		//logic web
 		fmt.Fprint(w, "Hello World")
 	}
-	
+
 	server := http.Server{
 		Addr:    "Localhost:8080",
 		Handler: handler,
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
+}
+func TestServeMux(t *testing.T) {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "Hello World")
+	})
+	mux.HandleFunc("/hi", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "Hi!")
+	})
+	server := http.Server{
+		Addr:    "Localhost:8080",
+		Handler: mux,
 	}
 
 	err := server.ListenAndServe()
