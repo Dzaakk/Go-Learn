900768adde2778b3c5519a1801e3bc8b9450730c
Dzkzzz <dzdzaakk@gmail.com>
2024-01-20 23:44:40 +0700

    context

--- 19_Go Fiber/fiber_test.go
+++ 19_Go Fiber/fiber_test.go
@@ -24,3 +24,29 @@ func TestRoutingHello(t *testing.T) {
 	assert.Equal(t, "Hello", string(bytes))
 
 }
+
+func TestCtx(t *testing.T) {
+	app := fiber.New()
+	app.Get("/hello", func(ctx *fiber.Ctx) error {
+		name := ctx.Query("name", "Guest")
+		return ctx.SendString("Hello " + name)
+	})
+
+	request := httptest.NewRequest("GET", "/hello?name=Dz", nil)
+	response, err := app.Test(request)
+	assert.Nil(t, err)
+	assert.Equal(t, 200, response.StatusCode)
+
+	bytes, err := io.ReadAll(response.Body)
+	assert.Nil(t, err)
+	assert.Equal(t, "Hello Dz", string(bytes))
+
+	request = httptest.NewRequest("GET", "/hello", nil)
+	response, err = app.Test(request)
+	assert.Nil(t, err)
+	assert.Equal(t, 200, response.StatusCode)
+
+	bytes, err = io.ReadAll(response.Body)
+	assert.Nil(t, err)
+	assert.Equal(t, "Hello Guest", string(bytes))
+}
