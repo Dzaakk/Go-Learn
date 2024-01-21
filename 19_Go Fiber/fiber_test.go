3f29753541dc97db5bbb1d31b961ec84f1a6067a
Dzkzzz <dzdzaakk@gmail.com>
2024-01-21 17:49:36 +0700

    http response

--- 19_Go Fiber/fiber_test.go
+++ 19_Go Fiber/fiber_test.go
@@ -244,3 +244,22 @@ func TestBodyParserXML(t *testing.T) {
 	assert.Nil(t, err)
 	assert.Equal(t, "Register Success dzk", string(bytes))
 }
+
+func TestResponseJSON(t *testing.T) {
+	app.Get("/user", func(ctx *fiber.Ctx) error {
+		return ctx.JSON(fiber.Map{
+			"username": "yatoyato",
+			"name":     "yato",
+		})
+	})
+
+	request := httptest.NewRequest("GET", "/user", nil)
+	request.Header.Set("Accept", "application/json")
+	response, err := app.Test(request)
+	assert.Nil(t, err)
+	assert.Equal(t, 200, response.StatusCode)
+
+	bytes, err := io.ReadAll(response.Body)
+	assert.Nil(t, err)
+	assert.Equal(t, `{"name":"yato","username":"yatoyato"}`, string(bytes))
+}
