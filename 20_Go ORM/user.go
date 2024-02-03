85b75532e6bcb10a34def2a43a0dc583e8314179
Dzkzzz <dzdzaakk@gmail.com>
2024-02-03 16:53:22 +0700

    field permission

--- 20_Go ORM/user.go
+++ 20_Go ORM/user.go
@@ -3,11 +3,12 @@ package learn_gorm
 import "time"
 
 type User struct {
-	ID        string    `gorm:"primary_key;column:id"`
-	password  string    `gorm:"column:password"`
-	Name      string    `gorm:"column:name"`
-	CreatedAt time.Time `gorm:"column:created_at;autoCreateTime"`
-	UpdatedAt time.Time `gorm:"column:name;autoCreateTime;autoUpdateTime"`
+	ID          string    `gorm:"primary_key;column:id;<-:create"` //create only
+	password    string    `gorm:"column:password"`
+	Name        string    `gorm:"column:name;<-"` //create and update
+	CreatedAt   time.Time `gorm:"column:created_at;autoCreateTime;<-:create"`
+	UpdatedAt   time.Time `gorm:"column:name;autoCreateTime;autoUpdateTime"`
+	Information string    `gorm:"-"` //no field on db so it will ignore this
 }
 
 func (u User) TableName() string {
